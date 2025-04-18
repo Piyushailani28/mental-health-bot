a7b763592922117aa4b13894959ed1e4f0d81564
Piyushailani28 <piyushailani34@gmail.com>
2025-04-18 11:40:13 +0530

    the api key changes

--- Backend/test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-google_api_key = "AIzaSyBQrTJFsVZ_Ydn5O8opGqL0MfZrVaCDj_U"
-genai.configure(api_key=google_api_key)
-gemini_model = genai.GenerativeModel("gemini-pro")
-
-for m in genai.list_models():
-    print(m.name)
